b1cc6f82010e23e233c8ec9213e0d55c372e6daf
Toms <11509310+tomzs@users.noreply.github.com>
2019-02-07 23:58:09 +0200

    Initial blockchain structure

---
+++ blockchain.py
@@ -0,0 +1,22 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        pass
+
+    def new_transaction(self):
+        pass
+
+
+    @staticmethod
+    def hash(block):
+        #Hash a block
+        pass
+    @property
+    def last_block(self):
+        #Returns last block in chain
+        pass
+
+
