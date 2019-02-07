634075741024f822566d0ef63cc2a96b0411df96
Toms <11509310+tomzs@users.noreply.github.com>
2019-02-08 00:20:55 +0200

    Blockchain representation

--- blockchain.py
+++ blockchain.py
@@ -1,22 +1,43 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        pass
+        #Genesis block
+        self.new_block(previous_hash = 1, proof = 100)
 
-    def new_transaction(self):
-        pass
+    def new_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.current_transactions = 0
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
 
 
     @staticmethod
     def hash(block):
         #Hash a block
-        pass
+        block_string = json.dumps(block, sort_keys=1).encode()
+        return hashlib.sha256(block_string).hexdigest()
     @property
     def last_block(self):
         #Returns last block in chain
-        pass
-
+        return self.chain[-1]
 
